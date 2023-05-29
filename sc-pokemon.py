82d1223bb54b68498b9cf5916bc323676d818ef6
sakikito <126849693+sakikito@users.noreply.github.com>
2023-05-29 23:34:49 +0900

    保存先を変更しました

--- sc-pokemon.py
+++ sc-pokemon.py
@@ -2,12 +2,27 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import urllib.request
+import os
+
+# Pythonで実行中のファイルの場所(パス)を取得する
+BASE_DIR = os.path.dirname(__file__)
+
+def get_output_dir_path():
+
+    # パスを結合する
+    output_dir_path = os.path.join(BASE_DIR, "output_pokemon")
+
+    if not os.path.exists(output_dir_path):
+
+        os.makedirs(output_dir_path)
+
+    return output_dir_path
 
 # Chromeの起動
 driver = webdriver.Chrome()
 driver.get("https://vercel-pokedex-gnticcdqg-kkml4220.vercel.app/")
 
-url = []
+urls = []
 for page in range(5):
 
     # cardを取得
@@ -22,7 +37,7 @@ for page in range(5):
         src = tag_pokemon.get_attribute('src')
 
         # urlを配列に格納
-        url.append(src)
+        urls.append(src)
 
     if page != 4:
         # 次ページに遷移
@@ -30,11 +45,16 @@ for page in range(5):
         elems_link[-1].click()
         time.sleep(5)
 
-for i in range(len(url)):
+output_dir_path = get_output_dir_path()
+index = 1
 
-    savename =  "C:/python_workspace/scraping/pokemon_image/" + "pokemon" + str(i) + ".png"
-    pokemon = urllib.request.urlopen(url[i]).read()
+for url in urls:
 
-    with open(savename, mode="wb") as f:
+    savename = f"pokemon_{str(index)}.jpg"
+    index += 1
+    pokemon = urllib.request.urlopen(url).read()
+
+    save_file_path = os.path.join(output_dir_path, savename)
+
+    with open(save_file_path, mode="wb") as f:
         f.write(pokemon)
-        print("保存しました")
